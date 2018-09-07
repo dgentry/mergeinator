7533acfee3d0124b401540e3cd5f478f186313ae
Dennis Gentry <dennis.gentry@gmail.com>
2018-09-07 10:08:20 -0700

    Export _dmark so I can test it.  There must be a better way.

--- mergeinator/__init__.py
+++ mergeinator/__init__.py
@@ -3,7 +3,8 @@
 
 from .mergeinator import WHT, GRN, YEL, RED, BLD, BLDWHT, BLDRED, NORMAL, RESET, DIM
 from .mergeinator import merge
+from .mergeinator import _dmark
 
 # Shut up pyflakes' F401 with explicit list of exported names
 __all__ = {WHT, GRN, YEL, RED, BLD, BLDWHT, BLDRED, NORMAL, RESET, DIM,
-           merge}
+           merge, _dmark}

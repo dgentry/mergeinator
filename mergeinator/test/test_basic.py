496497c9b494d1fb53df773fded6f7383d195b41
Dennis Gentry <dennis.gentry@gmail.com>
2018-09-07 10:10:02 -0700

    Use _dmark() instead of dmark().

--- mergeinator/test/test_basic.py
+++ mergeinator/test/test_basic.py
@@ -1,14 +1,14 @@
 import os
 
-from merginator import dmark
+from mergeinator import _dmark
 
 
 def test_dmark_identifies_dirs():
-    assert dmark(".") == "/"
+    assert _dmark(".") == "/"
 
 
 def test_dmark_identifies_plain_files():
     fn = "/tmp/fooby-bletch-xyzzy-666"
     open(fn, "w").close()
-    assert dmark(fn) == ""
+    assert _dmark(fn) == ""
     os.remove(fn)

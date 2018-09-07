8381a0dbe0e9e984a6d9229b7bc68e07b2fa68b5
Dennis Gentry <dennis.gentry@gmail.com>
2018-09-07 11:40:43 -0700

    Clearer name.

--- src/mergeinator/__init__.py
+++ src/mergeinator/__init__.py
@@ -2,9 +2,9 @@
 
 
 from .mergeinator import WHT, GRN, YEL, RED, BLD, BLDWHT, BLDRED, NORMAL, RESET, DIM
-from .mergeinator import merge
+from .mergeinator import do_merge
 from .mergeinator import _dmark
 
 # Shut up pyflakes' F401 with explicit list of exported names
 __all__ = {WHT, GRN, YEL, RED, BLD, BLDWHT, BLDRED, NORMAL, RESET, DIM,
-           merge, _dmark}
+           do_merge, _dmark}

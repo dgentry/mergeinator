f8bf7104df4b621ff0f5a3493927c1a09b75b6be
Dennis Gentry <dennis.gentry@gmail.com>
2021-04-18 17:53:57 -0700

    Add --version and Path src and dst

--- src/mergeinator/merge.py
+++ src/mergeinator/merge.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """CLI wrapper for mergeinator()"""
 
-import click
-from click import echo
+from click import command, argument, option, version_option, echo, Path
 import pkg_resources  # For version number
 from os.path import abspath
 from mergeinator import WHT, NORMAL, DIM, do_merge
 
 
-@click.command()
-@click.argument("source", required=True)
-@click.argument("destination", required=True)
-@click.option("-n", "--dryrun", help="Don't change anything", is_flag=True)
-@click.option("-y", "--yes", help="force answer of yes to questions.", is_flag=True)
-def cli(source, destination, dryrun, yes):
+@command()
+@argument("source", type=Path(exists=True))
+@argument("destination", type=Path(exists=True))
+@option("-n", "--dryrun", help="Don't change anything", is_flag=True)
+@option("-y", "--yes", help="force answer of yes to questions.", is_flag=True)
+@version_option()
+def cli(source, destination, dryrun, version, yes):
     """Merge trims away the source directory by moving its unique content into the destination
     directory.  Duplicate content is discarded.  It may take multiple runs of merge (one per
     level of subdirectory depth) to completely remove all duplicate content because merge
@@ -22,6 +22,7 @@ def cli(source, destination, dryrun, yes):
     """
     my_version = pkg_resources.require("mergeinator")[0].version
     echo(f"Mergeinator {my_version}")
-    echo(f"Merging {WHT}{source}{NORMAL} to {WHT}{destination}{NORMAL}\n")
-    echo(f"Full paths: {abspath(source)} to {abspath(destination)}\n")
-    do_merge(source, destination, 0, yes_flag=yes, dry_run_flag=dryrun)
+    if not version:
+        echo(f"Merging {WHT}{source}{NORMAL} to {WHT}{destination}{NORMAL}\n")
+        echo(f"Full paths: {abspath(source)} to {abspath(destination)}\n")
+        do_merge(source, destination, 0, yes_flag=yes, dry_run_flag=dryrun)
